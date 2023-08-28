23f1628ca7a60f6a35d60d29f96b36cf5700bd8a
Marios <marios_2510@hotmail.com>
2023-08-28 12:38:43 +0300

    Create order.py file. Define the Order Model Class.

---
+++ models/order.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+from database import db
+
+class Order(db.Model):
+    """Order model to store order data."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    total_price = db.Column(db.Float, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)

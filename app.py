bbd6295fce80273d6d0c4f7f413b4536a3ad4434
Marios <marios_2510@hotmail.com>
2023-08-28 13:29:58 +0300

    Update app.py. Setup a secret key. Initialise login_manager. Define the User Model class. Create a '/register' route.

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_bootstrap import Bootstrap5
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
 from database import db
-import models
+from forms import RegisterForm, LoginForm
 
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+app.config['SECRET_KEY'] = 'Secret123'
+
+Bootstrap5(app)
 
 db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, user_id)
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
 
 with app.app_context():
     db.create_all()
@@ -15,6 +35,31 @@ with app.app_context():
 @app.route('/')
 def index():
     return "Welcome to our eCommerce site!"
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        result = db.session.execute(db.select(User).where(User.email == email))
+        user = result.scalar()
+
+        # if user:
+        #     flash("You've already signed up with this email, login instead.")
+        #     return redirect(url_for('login'))
+
+        hashed_password = generate_password_hash(
+            form.password.data, method='pbkdf2:sha256', salt_length=8)
+        new_user = User(
+            email=form.email.data,
+            name=form.name.data,
+            password=hashed_password,
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user)
+        return redirect(url_for('index'))
+    return render_template("register.html", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)

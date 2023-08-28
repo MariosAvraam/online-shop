c675953c488d26f6af16e4d9c47bfcfe86b450bd
Marios <marios_2510@hotmail.com>
2023-08-28 14:29:32 +0300

    Update cart.py Update user_id field. Add product field.

--- models/cart.py
+++ models/cart.py
@@ -2,8 +2,8 @@ from database import db
 
 class Cart(db.Model):
     """Cart model to store cart data."""
-
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    product = db.relationship('Product', backref='carts')
     quantity = db.Column(db.Integer, default=1, nullable=False)

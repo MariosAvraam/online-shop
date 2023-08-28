21b3ae4c89ebdb91f25f2a1dbb256809339389dc
Marios <marios_2510@hotmail.com>
2023-08-28 14:28:55 +0300

    Refactor product.py

--- models/product.py
+++ models/product.py
@@ -2,7 +2,6 @@ from database import db
 
 class Product(db.Model):
     """Product model to store product data."""
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     description = db.Column(db.Text, nullable=True)

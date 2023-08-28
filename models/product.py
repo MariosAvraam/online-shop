f465ad64d917819bbda6cc144725491f14ae0fd0
Marios <marios_2510@hotmail.com>
2023-08-28 12:39:02 +0300

    Create product.py file. Define Product Model Class.

---
+++ models/product.py
@@ -0,0 +1,10 @@
+from database import db
+
+class Product(db.Model):
+    """Product model to store product data."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    price = db.Column(db.Float, nullable=False)
+    image_url = db.Column(db.String(300), nullable=True)

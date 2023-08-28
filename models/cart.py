183da9fbc04e8272eadbf8b393a0d1f6745f4982
Marios <marios_2510@hotmail.com>
2023-08-28 12:38:20 +0300

    Create cart.py file. Define the Cart Model Class.

---
+++ models/cart.py
@@ -0,0 +1,9 @@
+from database import db
+
+class Cart(db.Model):
+    """Cart model to store cart data."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    quantity = db.Column(db.Integer, default=1, nullable=False)

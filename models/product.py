5d9db776dbc7943404624985a0ec6df30c2057df
Marios <marios_2510@hotmail.com>
2023-08-30 15:28:31 +0300

    Refactor product.py.

--- models/product.py
+++ models/product.py
@@ -1,7 +1,18 @@
 from database import db
 
 class Product(db.Model):
-    """Product model to store product data."""
+    """
+    Product model to store product data available for purchase.
+    
+    Attributes:
+        id (int): Unique identifier for each product.
+        name (str): Name of the product.
+        description (str): Description or details about the product.
+        price (float): Price of the product.
+        image_url (str): URL of the product's image.
+        cart_items (relationship): Relationship to the Cart model.
+    """
+    
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     description = db.Column(db.Text, nullable=True)

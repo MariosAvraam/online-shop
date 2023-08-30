f9d8fecb8645d518559d14a1fa7734fce2f27a41
Marios <marios_2510@hotmail.com>
2023-08-30 15:29:40 +0300

    Refactor forms.py

--- forms.py
+++ forms.py
@@ -3,17 +3,23 @@ from wtforms import StringField, SubmitField, PasswordField, TextAreaField, Floa
 from wtforms.validators import DataRequired, Email, Optional, URL
 
 class RegisterForm(FlaskForm):
+    """Form for user registration."""
+    
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     name = StringField('Name', validators=[DataRequired()])
     submit = SubmitField('Sign Up')
 
 class LoginForm(FlaskForm):
+    """Form for user login."""
+    
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Log In')
 
 class ProductForm(FlaskForm):
+    """Form for adding a new product."""
+    
     name = StringField('Product Name', validators=[DataRequired()])
     description = TextAreaField('Description', validators=[Optional()])
     price = FloatField('Price', validators=[DataRequired()])
@@ -21,6 +27,8 @@ class ProductForm(FlaskForm):
     submit = SubmitField('Add Product')
 
 class EditProductForm(FlaskForm):
+    """Form for editing an existing product."""
+    
     name = StringField('Product Name', validators=[DataRequired()])
     description = TextAreaField('Description', validators=[Optional()])
     price = FloatField('Price', validators=[DataRequired()])

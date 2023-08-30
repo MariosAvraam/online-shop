6e0702ddff3f0f4d7b17deb340ccb4bba15970f3
Marios <marios_2510@hotmail.com>
2023-08-30 15:28:04 +0300

    Refactor cart.py

--- models/cart.py
+++ models/cart.py
@@ -1,7 +1,17 @@
 from database import db
 
 class Cart(db.Model):
-    """Cart model to store cart data."""
+    """
+    Cart model to store cart data for each user.
+    
+    Attributes:
+        id (int): Unique identifier for each cart entry.
+        user_id (int): ID of the user to whom the cart entry belongs.
+        product_id (int): ID of the product in the cart entry.
+        product (relationship): Relationship to the Product model.
+        quantity (int): Number of units of the product in the cart.
+    """
+    
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
